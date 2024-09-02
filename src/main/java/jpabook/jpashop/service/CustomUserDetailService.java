86343fa183f7ff4b7084cdb0f5d833e7f0abc63d
jaehyun <yjh454@naver.com>
2024-09-02 17:09:58 +0900

    jwt소스 미반영 소스 추가 머지

---
+++ src/main/java/jpabook/jpashop/service/CustomUserDetailService.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자 x"));
+    }
+}
